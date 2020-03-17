1c99a8da4abe7940b6647e7b82f2b931e509d124
leicareddot <leicareddot@gmail.com>
2020-03-18 05:25:52 +0900

    Spring MVC - Resource Handler

---
+++ spring-mvc-resource-handler/src/main/java/com/atoz_develop/springmvcresourcehandler/SpringMvcResourceHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.atoz_develop.springmvcresourcehandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcResourceHandlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcResourceHandlerApplication.class, args);
+    }
+
+}
